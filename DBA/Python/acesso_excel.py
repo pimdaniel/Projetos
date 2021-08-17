a45bdb1c885cb491ea8218b89783c4e911c6f6b1
pimdaniel <pimdaniel@gmail.com>
2021-08-17 02:06:45 -0300

    Adicao excel file

---
+++ DBA/Python/acesso_excel.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+
+path = r"C:\Claro\Desenvolvimento\test.xlsx"
+
+wb = load_workbook(path)
+
+ws =wb.worksheets[0]
+ws.cell(row=1, column=1, value='SQL SERVER')
+ 
+wb.save(path)
+ 
+  
+ 
+ 
---
+++ DBA/Python/acesso_excel2.py
@@ -0,0 +1,24 @@
+# -----------------------------------------------------------------------------------------------------------------------------
+path = r"C:\Claro\Desenvolvimento\XXXXXXX.xlsx"
+# Cria um excel a partir de uma query  em um database
+
+#Cria um DataFrame
+df = pd.DataFrame({'ususarios': [],'host': []})
+
+#cria conexao com o banco
+conn =mysqlConn()
+cur = conn.cursor()
+
+#Query a ser executada
+sql="select user,host from mysql.user;"
+
+#Abre um cursor para a query
+cur.execute(sql)
+
+for itens in cur: 
+    #print(str(itens[0]))
+    df = df.append({'ususarios': str(itens[0]),'host': str(itens[1]) }, ignore_index=True)   
+conn.close()
+
+df.to_excel(path,sheet_name='mysqlUsers',index=False)
+# -----------------------------------------------------------------------------------------------------------------------------
