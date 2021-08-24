d8c643ba157011bf5c290d77addc16c641ee9f6d
pimdaniel <pimdaniel@gmail.com>
2021-08-24 15:36:22 -0300

    mudanca grande

---
+++ Python/acesso_Mysql.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append("../")
+from DAL.CONN import mysqlConn,bancoMysql
+from LOG.logs_APP import logDatabase
+
+#configurando o los dos erros de banco de dados
+_log,_logFile  = logDatabase()
+ 
+
+# ----------------------- Maneira 01---------------------------------------
+'''
+conn =mysqlConn()
+cur = conn.cursor()
+
+sql="select user from mysql.user;"
+
+cur.execute(sql)
+
+
+for itens in cur: 
+    print(str(itens[0]))
+    
+conn.close()
+'''
+# ----------------------- Maneira 02 ---------------------------------------
+
+try: 
+    with bancoMysql("10.243.172.57","mysql") as cur:         
+       
+        sql = "SELECT user,host FROM mysql.user;"
+        cur.execute(sql)
+        
+        #query = list(cur.fetchall())        
+        
+        for itens in cur: 
+            print(str(itens[0]))
+
+except Exception as e:
+   #_log.error(e)      
+   print(e)
